644ed371a2062f8fe81eb0a07e05590cdd115393
MrOkayWang <2579381472@qq.com>
2019-05-18 21:50:22 +0800

    添加了介绍
    添加了关于网站介绍的网站 'about.html'
    并且在pro的urls里配置了路由

--- pro/urls.py
+++ pro/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', function.home),
     path('count/', function.count),
+    path('about/', function.about),
     path('myfirstapp/', include('myfirstapp.urls'))
 ]
